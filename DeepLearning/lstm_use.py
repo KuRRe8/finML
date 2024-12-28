597ebac668dcfd5ef32525682438e7ad2a846220
nscc_yang0886 <yang0886@e.ntu.edu.sg>
2024-12-28 12:38:31 +0800

    first edition of LSTM

--- DeepLearning/lstm_use.py
+++ DeepLearning/lstm_use.py
@@ -28,7 +28,7 @@ _DEBUG_FALSE = False                                        # Always False
 
 UNIQUE_THRESHOLD = 1                                      # for each feature, at least this number of unique values considered as good for discrimination, should be small number
 FEATURE_SELECTION_RATIO = 0.1                               # for each feature, those process less than this ratio of values will be removed 0.7*400k=280k
-YEAR_LIMIT = 3
+YEAR_LIMIT = 8
 
 TARGET_NAME = 'xrd'                                         # target column name
 PRESERVE_LIST = []
@@ -110,10 +110,95 @@ origin_imputed = pd.DataFrame(origin_imputed, columns=origin.columns)
 
 target_imputed = imputer_target_col.fit_transform(target_col.values.reshape(-1,1))
 
-grouped_by_fyear = origin.groupby('gvkey',group_keys=True)
+imputed = pd.concat([origin_imputed, pd.DataFrame(target_imputed, columns=[TARGET_NAME])], axis=1)
 
-for key, group in grouped_by_fyear:
+grouped_by_gv = imputed.groupby('gvkey',group_keys=True)
+
+for key, group in grouped_by_gv:
     if len(group) < YEAR_LIMIT:
-        origin = origin[origin['gvkey'] != key]
+        imputed = imputed[imputed['gvkey'] != key]
 
-grouped_by_fyear_1 = origin.groupby('gvkey',group_keys=True)
+imputed.to_csv(os.path.join(pathprefix,'temp',f'grouped_with_{YEAR_LIMIT}plus.csv'), index=False)
+
+grouped_by_gv_1 = imputed.groupby('gvkey', group_keys=True)
+
+all_filled_data = []
+first_100_test_groups = []
+other_groups = []
+
+for key, group in grouped_by_gv_1:
+    group = group.set_index('fyear').sort_index()
+    full_range = range(1988, 2025)
+    group = group.reindex(full_range)
+    group['mask4lstm'] = group['gvkey'].isnull()
+    group['gvkey'] = group['gvkey'].ffill().bfill()
+    group = group.ffill().bfill()
+    group.fillna(0, inplace=True)
+    all_filled_data.append(group)
+
+first_100_test_groups = all_filled_data[:100]
+other_groups = all_filled_data[100:]
+
+all_filled_data = pd.concat(all_filled_data).reset_index().rename(columns={'index': 'fyear'})
+all_filled_data.to_csv(os.path.join(pathprefix,'temp','filled_data.csv'), index=False)
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+lstm = lstm_def.myLSTM(input_size=all_filled_data.shape[1]-3, hidden_size=64, output_size=37, num_layers=1).to(device)
+criterion = nn.MSELoss().to(device)
+optimizer = optim.Adam(lstm.parameters(), lr=0.001)
+
+train_data = []
+train_target = []
+#train_mask = []
+for group in other_groups:
+    train_data.append(group.drop(columns=['gvkey', 'mask4lstm']).values)
+    train_target.append(group[TARGET_NAME].values)
+    #train_mask.append(group['mask4lstm'].values)
+
+train_data = torch.tensor(train_data, dtype=torch.float32).to(device)
+train_target = torch.tensor(train_target, dtype=torch.float32).to(device)
+#train_mask = torch.tensor(train_mask, dtype=torch.bool).to(device)
+
+test_data = []
+test_target = []
+#test_mask = []
+for group in first_100_test_groups:
+    test_data.append(group.drop(columns=['gvkey', 'mask4lstm']).values)
+    test_target.append(group[TARGET_NAME].values)
+    #test_mask.append(group['mask4lstm'].values)
+
+test_data = torch.tensor(test_data, dtype=torch.float32).to(device)
+test_target = torch.tensor(test_target, dtype=torch.float32).to(device)
+#test_mask = torch.tensor(test_mask, dtype=torch.bool).to(device)
+
+train_data, val_data, train_target, val_target = train_test_split(
+    train_data, train_target, test_size=0.2, random_state=42)
+
+for epoch in range(100):
+    lstm.train()
+    optimizer.zero_grad()
+    output = lstm(train_data)
+    loss = criterion(output, train_target)
+    loss.backward()
+    optimizer.step()
+    print(f'Epoch {epoch} loss: {loss.item()}')
+
+lstm.eval()
+val_output = lstm(val_data)
+val_loss = criterion(val_output, val_target)
+print(f'Validation loss: {val_loss.item()}')
+
+test_output = lstm(test_data)
+test_loss = criterion(test_output, test_target)
+print(f'Test loss: {test_loss.item()}')
+
+test_output_cpu = test_output.cpu()
+test_output_np = test_output_cpu.detach().numpy()
+test_target_cpu = test_target.cpu()
+test_target_np = test_target_cpu.detach().numpy()
+
+print(f'Test RMSE: {sqrt(mean_squared_error(test_target_np, test_output_np))}')
+print(f'Test R2: {r2_score(test_target_np, test_output_np)}')
+
+logger.info('Finished.')
